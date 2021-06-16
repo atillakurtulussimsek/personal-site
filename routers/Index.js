d92ff505f39c48ffa56a9cee51dee1fb00043dfa
Atilla K. ŞİMŞEK <info@atillakurtulussimsek.com.tr>
2021-06-17 02:21:14 +0300

    database status check

--- routers/Index.js
+++ routers/Index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('*', async function(req,res,next){
+    if(DatabaseStatus == true) return next();
+
+    
+});
+
 router.get('/', async function(req,res){
     return res.render('index');
 });
