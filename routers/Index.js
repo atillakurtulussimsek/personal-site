31c47bc26bf3bce8e32b4485ddf739bbf8674ee3
Atilla K. ŞİMŞEK <info@atillakurtulussimsek.com.tr>
2021-06-17 08:57:53 +0300

    database error check router added

--- routers/Index.js
+++ routers/Index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.all('*', async function(req,res,next){
     if(DatabaseStatus == true) return next();
 
-    
+    res.render('error', { statusCode: 500, message: 'Bir sorunla karşılaştık. Lütfen daha sonra tekrar deneyin.'});
 });
 
 router.get('/', async function(req,res){
