dbe9442a4aed6bc9aef2885424005b57ce0cfcfb
Atilla K. ŞİMŞEK <info@atillakurtulussimsek.com.tr>
2021-06-17 09:10:28 +0300

    Config first release now published

---
+++ Config_example.js
@@ -0,0 +1,15 @@
+module.exports = {
+    WebServer: {
+        Port: ''
+    },
+    Data: {
+        Status: true, // true or false
+        URL: '',
+        IP: '',
+        Port: '',
+        Name: ''
+    },
+    Auth: {
+        SessionSecret: ''
+    }
+}
