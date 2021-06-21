c0df926b70fb0ba8bd7f9151dd6731ede2bc5b37
Atilla K. ŞİMŞEK <info@atillakurtulussimsek.com.tr>
2021-06-21 20:34:25 +0300

    deleted

--- routers/Index.js
+++ routers/Index.js
@@ -2,17 +2,13 @@ const express = require('express');
 const router = express.Router();
 const Config = require('../Config');
 
-router.all('*', async function (req, res, next) {
-    if (DatabaseStatus == true) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-        return;
-    }
+router.all('*', async function(req,res,next){
+    if(DatabaseStatus == true) return next();
 
-    res.render('error', { statusCode: 500, message: 'Bir sorunla karşılaştık. Lütfen daha sonra tekrar deneyin.' });
+    res.render('error', { statusCode: 500, message: 'Bir sorunla karşılaştık. Lütfen daha sonra tekrar deneyin.'});
 });
 
-router.get('/', async function (req, res) {
+router.get('/', async function(req,res){
     return res.render('index', {
         CDN: Config.WebServer.cdnURL
     });
