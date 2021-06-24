f4346b0d3e6750ed952e2e596412e9543e73c040
Atilla K. ŞİMŞEK <info@atillakurtulussimsek.com.tr>
2021-06-25 01:36:37 +0300

    It was decided to restart the project for infrastructure change.

--- routers/Admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Config = require('../Config');
-
-router.all('*', async function(req,res){
-    if(req.session.isAdminLogin == false) return res.redirect('/auth/login');
-}) 
-
-module.exports = router;
